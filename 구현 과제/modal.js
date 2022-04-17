068c04020ab7049a8bec3adb6f02b7cd4b7a7902
yeodahui <yeodahui.dev@gmail.com>
2022-04-17 20:47:32 +0900

    간소한 수정

--- 구현 과제/modal.js
+++ 구현 과제/modal.js
@@ -35,16 +35,19 @@ if (loginBtn) {
     if (id.value) {
       console.log("아이디 인식되었음");
 
-      id.style.border = "1px solid #C4C4C4;";
+      // 비밀번호 확인
+      if (true) {
+        const notice = document.createElement("div");
 
-      const notice = document.createElement("div");
+        notice.classList.add("notice");
+        notice.innerHTML = "아이디 혹은 비밀번호가 일치하지 않습니다.";
 
-      notice.classList.add("notice");
-      notice.innerHTML = "아이디 혹은 비밀번호가 일치하지 않습니다.";
-      notice.style.margin = "6px 0 12px 3px;";
-
-      if (pw.nextSibling.innerHTML != notice.innerHTML) {
-        parent.insertBefore(notice, pw.nextSibling);
+        // 문구 중복 출력 방지
+        if (pw.nextSibling.innerHTML != notice.innerHTML) {
+          parent.insertBefore(notice, pw.nextSibling);
+        }
+      } else {
+        console.log("로그인 성공");
       }
     } else {
       const notice = document.createElement("div");
@@ -52,6 +55,7 @@ if (loginBtn) {
       notice.classList.add("notice");
       notice.innerHTML = "아이디를 입력해 주세요.";
 
+      // 문구 중복 출력 방지
       if (id.nextSibling.innerHTML != notice.innerHTML) {
         parent.insertBefore(notice, id.nextSibling);
         id.style.border = "2px solid #F4492E";
